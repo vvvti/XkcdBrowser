c8ae6c5d3050e1a4f6477d60028031dfb3c54339
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-07-11 19:42:33 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>XKCD</Text>
+    </View>
+  );
+};
+
+export default App;
